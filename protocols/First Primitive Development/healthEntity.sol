4737b118516c951f02382c9550480aa43ba88a43
lokithe5th <lourensmba@gmail.com>
2022-03-10 10:23:39 +0200

    Added events related to new HEs, verification, rep

--- protocols/First Primitive Development/healthEntity.sol
+++ protocols/First Primitive Development/healthEntity.sol
@@ -18,13 +18,22 @@ import "@openzeppelin/contracts@4.5.0/utils/Counters.sol";
 contract HealthEntity is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
     using Counters for Counters.Counter;
 
+    //  Events
+    event newHealthEntity(uint32 tokenId, address tokenHolder, uint32 healthEntityType);
+    event healthEntityVerified(uint32 tokenId, uint32 sponsor);
+    event revokeVerification(uint32 tokenId, uint32 auditor);
+    event healthEntityModified(uint32 tokenId, uint32 auditor);
+    event reputationEvent(uint32 tokenId, uint32 reputationChange);
+    event verificationRequest(uint32 tokenId);
+
+    //  Struct containing Health Entities
     struct healthEntity {
-        uint32 healthEntityType;     // From list of Entity types: MD, Physio, Occ Ther, Nursing etc
-        bytes32 healthId;            // Keccak256 hash of ID provided by health professional's statutory body
-        bool verified;              // Has this Health Entity been verified by another Health Entity?
-        address sponsor;            // Who has verified this Health Entity
-        bool active;                // Is Health Entity active in health system at the moment?
-        uint32 reputation;           // Reputation accrued by Health Entity
+        uint32 healthEntityType;        //  From list of Entity types: MD, Physio, Occ Ther, Nursing etc
+        bytes32 healthId;               //  Keccak256 hash of ID provided by health professional's statutory body
+        bool verified;                  //  Has this Health Entity been verified by another Health Entity?
+        address sponsor;                //  Who has verified this Health Entity
+        bool active;                    //  Is Health Entity active in health system at the moment?
+        uint32 reputation;              //  Reputation accrued by Health Entity
     }
 
     healthEntity[] public healthEntities;
@@ -65,6 +74,8 @@ contract HealthEntity is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC7
             active: true,
             reputation: 0
         }));
+
+        emit newHealthEntity(tokenId, to, _type);
     }
 
     //Create the first healthEntity. Must be a doxxed entity in order to be held accountable.
@@ -100,7 +111,7 @@ contract HealthEntity is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC7
         healthEntities[_targetEntity].sponsor = msg.sender;
 
         //Reputation is gained for verifying other healthEntities
-        healthEntities[_verifyingEntity].reputation++;
+        _incrementReputation(_verifyingEntity, 1);
     }
 
     //Auditor actions. Only to be used by Auditor multisig. The multisig become the sponsor.
@@ -142,22 +153,37 @@ contract HealthEntity is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC7
         _setNotActive(tokenId);
     }
 
-    // Sets target NFT to not verified
+    //  Internal Functions
+    //  Function to increase reputation
+    function _incrementReputation(uint32 _targetId, uint32 _incrementBy) internal {
+        require(_incrementBy > 0);
+        uint32 _reputation =  healthEntities[_targetId].reputation;
+        healthEntities[_targetId].reputation = _reputation + _incrementBy;
+
+        emit reputationEvent(_targetId, _incrementBy);
+    }
+
+    //  Sets target NFT to not verified
     function _revokeVerification(uint256 _tokenId) internal {
         healthEntities[_tokenId].verified = false;
+
+        emit revokeVerification(_tokenId, msg.sender);
     }
     
-    // Sets target NFT to not active
+    //  Sets target NFT to not active
     function _setNotActive(uint256 _tokenId) internal {
         healthEntities[_tokenId].active = false;
     }
 
-    // Method to allow someone to verify a health entity without storing direct info on chain. Contains a keccak256 hash of JSON format with governing body details and unique identifier
+    //  Method to allow someone to verify a health entity without storing direct info on chain. Contains a keccak256 hash of JSON format with governing body details and unique identifier
     function verifyHealthEntity(uint32 _targetId, string memory _verificationString) public view returns (bool) {
-        require(healthEntities[_targetId].healthId == keccak256(_verificationString));
+        emit verificationRequest(_targetId);
+        require(_targetId > 0, "tokenId must be higher than 0");
+        require(healthEntities[_targetId].healthId == keccak256(_verificationString), "Supplied information does not match records");
+        return true;
     }
 
-    // The following functions are overrides required by Solidity.
+    //  The following functions are overrides required by Solidity.
 
     function supportsInterface(bytes4 interfaceId)
         public
